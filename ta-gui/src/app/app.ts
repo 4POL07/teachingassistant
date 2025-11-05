a0591e1d5ffc0015b0f738743b24d15d97cdb150
4POL07 <polomoonchild@gmail.com>
2025-11-05 11:41:01 -0300

    cherry-pick elementos do formulario para cadastro de alunos

--- ta-gui/src/app/app.ts
+++ ta-gui/src/app/app.ts
@@ -6,6 +6,15 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.css'
 })
+
 export class App {
   protected readonly title = signal('ta-gui');
+  aluno: Aluno = {nome: "", cpf: "", email: ""};
+}
+
+
+export class Aluno {
+  nome: string;
+  cpf: string;
+  email: string;
 }
